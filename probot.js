f9d08e20613261dc738b06faf992fe0b629230fa
_ShaDoW_MaN_ <32039975+ShadwX57@users.noreply.github.com>
2018-10-13 08:18:52 +0300

    Update probot.js

--- probot.js
+++ probot.js
@@ -1133,17 +1133,7 @@ m.delete();
 omar.reply("`تم حذف جميع الرتب بنجاح`")
 }
 });
-client.on('message', message => {
-    if (message.content === "#inv") {
-        if(!message.channel.guild) return;
-    let embed = new Discord.RichEmbed()
-    .setAuthor(` ${message.author.username} `, message.author.avatarURL)      
-    .setTitle(`Click Here To Add Probot `)
-    .setURL(`https://discordapp.com/api/oauth2/authorize?client_id=498924229383946260&permissions=8&scope=bot`)
-    .setThumbnail(" https://cdn.discordapp.com/avatars/377904849783750667/6c76e412f18c142dfd711d05fb363869.png?size=2048")        
- message.channel.sendEmbed(embed);
-   }
-});
+
 
 client.on('message', message => {
   if (message.author.bot) return;
