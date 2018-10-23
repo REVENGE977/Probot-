8a45490a1381d23a98ba0a8f7e5308a61b79819c
Discord.js Arabic Developer <32039975+ShadwX57@users.noreply.github.com>
2018-10-23 21:31:01 +0300

    Update probot.js

--- probot.js
+++ probot.js
@@ -5,7 +5,8 @@ const dateFormat = require('dateformat');
 const client = new Discord.Client(); 
 const Canvas = require("canvas"); 
 const prefix = "#"
-const id = JSON.parse(fs.readFileSync("./id/rank.json", "utf8"));
+const id = JSON.parse(fs.readFileSync("./id/rank.json", "utf8"))
+	
 client.on("message", message => {
   if (message.author.bot) return;
 fs.writeFile('./id/rank.json', JSON.stringify(id), (err) => {
